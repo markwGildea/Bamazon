e29047bc3e51b291a76f148b1ee6a51d34c49acc
Mark Gildea <gildeamark1@gmail.com>
2017-01-12 14:33:02 -0600

    dependencies

---
+++ bamazonCustomer.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+var inquire = require('inquirer');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    user: "root",
+
+    password: "mgof5562",
+    database: "Bamazon"
+});
+
+
+
